60528b3f33a8fd82a3c256831025f4bf1e79a529
BugHunter-X <askzenai@gmail.com>
2025-01-02 09:12:05 +0100

    Add bug.java file

---
+++ bug.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int index = 10; // Index out of bounds
+        System.out.println(array[index]);
+    }
+}
